1f99539a5cf96361ad8ba34c2e8b7ae685b1d3c3
Tristan Gingold <tgingold@free.fr>
2020-11-27 07:42:28 +0100

    Freeze fd_ms DRC:6, Ant:0

--- openlane/fd_ms/config.tcl
+++ openlane/fd_ms/config.tcl
@@ -10,10 +10,10 @@ source $script_dir/../fd-common/fd-config.tcl
 
 # area: 90*.46  120*2.72
 set ::env(FP_SIZING) absolute
-set x [expr 1210 * 0.46]
+set x [expr 1160 * 0.46]
 set ::env(DIE_AREA) "0 0 $x 231.2"
 
-set ::env(PL_TARGET_DENSITY) 0.50
+set ::env(PL_TARGET_DENSITY) 0.45
 
 # set ::env(PL_RESIZER_OVERBUFFER) 1
 set ::env(DIODE_INSERTION_STRATEGY) 0
@@ -24,11 +24,11 @@ set ::env(DIODE_INSERTION_STRATEGY) 0
 # -> y = 18810
 set chan [open $script_dir/macro_placement.cfg w]
 
-set x [expr 340 * 0.46]
+set x [expr 290 * 0.46]
 puts $chan "inst_tdelay_line $x 18.81 N"
-set x [expr 628 * 0.46]
+set x [expr 578 * 0.46]
 puts $chan "inst_idelay_line $x 18.81 N"
-set x [expr 930 * 0.46]
+set x [expr 880 * 0.46]
 puts $chan "inst_rdelay_line $x 18.81 N"
 
 close $chan
