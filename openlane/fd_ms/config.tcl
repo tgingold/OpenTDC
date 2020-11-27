3939bc2079805a77e723e2d1eed605270142b609
Tristan Gingold <tgingold@free.fr>
2020-11-27 07:32:20 +0100

    fd_ms: new config (drc:2, ant:3)

--- openlane/fd_ms/config.tcl
+++ openlane/fd_ms/config.tcl
@@ -8,9 +8,27 @@ set macros [list "delayline_9_ms"]
 
 source $script_dir/../fd-common/fd-config.tcl
 
+# area: 90*.46  120*2.72
 set ::env(FP_SIZING) absolute
-set ::env(DIE_AREA) "0 0 280 280"
+set x [expr 1210 * 0.46]
+set ::env(DIE_AREA) "0 0 $x 231.2"
+
+# set ::env(PL_RESIZER_OVERBUFFER) 1
+set ::env(DIODE_INSERTION_STRATEGY) 0
+
+# Macros
+# 9_hs VPWR: 7.680-9.280 -> y = 8.480
+# prj  VPWR: 27290
+# -> y = 18810
+set chan [open $script_dir/macro_placement.cfg w]
+
+set x [expr 340 * 0.46]
+puts $chan "inst_tdelay_line $x 18.81 N"
+set x [expr 628 * 0.46]
+puts $chan "inst_idelay_line $x 18.81 N"
+set x [expr 930 * 0.46]
+puts $chan "inst_rdelay_line $x 18.81 N"
+
+close $chan
 
 set ::env(MACRO_PLACEMENT_CFG) $script_dir/macro_placement.cfg
-
-set ::env(PL_RESIZER_OVERBUFFER) 1
