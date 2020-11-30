e091d4c40cac2b29061b5e5cebf251af9dbd96ca
Tristan Gingold <tgingold@free.fr>
2020-11-30 14:12:43 +0100

    fd_inline_1: make it higher drc:0 ant:0

--- openlane/fd_inline_1/config.tcl
+++ openlane/fd_inline_1/config.tcl
@@ -9,8 +9,9 @@ set macros [list]
 source $script_dir/../fd-common/fd-config.tcl
 
 set ::env(FP_SIZING) absolute
-set ::env(DIE_AREA) "0 0 280 108.8"
+set ::env(DIE_AREA) "0 0 280 136.0"
 
 set ::env(FP_PIN_ORDER_CFG) $script_dir/../fd_hd_25_1/pin_order.cfg
 
 set ::env(PL_TARGET_DENSITY) 0.40
+set ::env(DESIGN_IS_CORE) 0
