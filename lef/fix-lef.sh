c8a0b90f850128e5cad651738ad2b361ba302fce
Tristan Gingold <tgingold@free.fr>
2020-12-05 11:10:02 +0100

    fix-lef: add spdx, make it more handy

--- lef/fix-lef.sh
+++ lef/fix-lef.sh
@@ -1,1 +1,15 @@
-for f in *.lef.orig; do b=`basename $f .lef.orig`; PYTHONPATH=../../openlane/scripts/spef_extractor/ python3 ../tools/fix_power_pin.py --def_file=../def/$b.def --lef_file=$b.lef.orig --layer=met4 --pin VPWR VGND -o $b.lef.new; done
+#!/bin/sh
+# Script to add power stripes to pin
+#
+# SPDX-FileCopyrightText: (c) 2020 Tristan Gingold <tgingold@free.fr>
+# SPDX-License-Identifier: Apache-2.0
+
+for f in $*; do
+    b=`basename $f .lef`;
+    echo "Copying $f as $f.orig"
+    mv $f $f.orig
+    echo "Fixing $f"
+    PYTHONPATH=../../openlane/scripts/spef_extractor/ python3 ../tools/fix_power_pin.py --def_file=../def/$b.def --lef_file=$b.lef.orig --layer=met4 --pin VPWR VGND -o $f;
+    echo "Diffs:"
+    diff -w $f.orig $f
+done
