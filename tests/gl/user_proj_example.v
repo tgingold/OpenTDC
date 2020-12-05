6582d3cb75faf10fcfcc6758bb59df17960a1678
Tristan Gingold <tgingold@free.fr>
2020-12-05 11:09:35 +0100

    Move gl files, add gate-level simulation

---
+++ tests/gl/user_proj_example.v
@@ -0,0 +1,4 @@
+/*
+/ SPDX-FileCopyrightText: (c) 2020 Tristan Gingold <tgingold@free.fr>
+/ SPDX-License-Identifier: Apache-2.0
+*/
---
+++ tests/gl/user_project_wrapper.v
@@ -0,0 +1,10 @@
+/*
+/ SPDX-FileCopyrightText: (c) 2020 Tristan Gingold <tgingold@free.fr>
+/ SPDX-License-Identifier: Apache-2.0
+*/
+
+`include "blackbox.v"
+`include "../verilog/gl/zero.synthesis_preroute.v"
+//`include "../verilog/gl/wb_interface.lvs.powered.v"
+`include "wb_interface.simple.v"
+`include "../verilog/gl/user_project_wrapper.power.v"
